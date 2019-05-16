a1d9b17e4c4d86564cb97507b2f58ffd04cc5db0
Angela Lo <angelawenlo@gmail.com>
2019-05-16 20:37:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,7 @@ def adding
   shipping_manifest
   # add 2 muskets to the shipping_manifest hash below
 
-  shipping_manifest << ["muskets"] = 2
+  shipping_manifest ["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
